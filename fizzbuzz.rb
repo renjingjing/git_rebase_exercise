c5001767baacf209d67519d2873d2dca7ba7d65c
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-11-09 08:12:44 -0800

    take input from user instead of using just FIZZ and BUZZ

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,16 @@
+print "Give me a word (e.g. FIZZ): "
+first_word = gets.chomp
+
+print "Give me a word (e.g. BUZZ): "
+second_word = gets.chomp
+
 for x in 1..100
   if x % 3 == 0 && x % 5 == 0
-    puts "FIZZBUZZ"
+    puts "#{first_word}#{second_word}"
   elsif x % 5 == 0
-    puts "BUZZ"
+    puts second_word
   elsif x % 3 == 0
-    puts "FIZZ"
+    puts first_word
   else
     puts x
   end
