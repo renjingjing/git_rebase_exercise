38816e28878d82f8791374202dc8756dac6f97e5
renjingjing <renjingjing@gmail.com>
2015-11-09 15:21:02 -0800

    hard_push

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ print "Give me a word (e.g. FIZZBUZZ): "
 second_word = gets.chomp
 
 for x in 1..100
-  if x % 3 == 0 && x % 5 == 0
+  if x % 15 == 0 && x % 15 == 0
     puts "#{first_word}#{second_word}"
   elsif x % 5 == 0
     puts second_word
